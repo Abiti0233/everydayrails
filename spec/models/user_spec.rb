47cc39de4362eed83c203703b5b6755d1a683d8e
Kotaro Ito <96063584+Abiti0233@users.noreply.github.com>
2023-10-19 18:26:35 +0900

    Update spec/models/user_spec.rb
    Co-authored-by: koki-takishita <48609992+koki-takishita@users.noreply.github.com>

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-    it "has a valid factory" do
-      expect(FactoryBot.build(:user)).to be_valid
-    end
+  it "has a valid factory" do
+    expect(FactoryBot.build(:user)).to be_valid
+  end
 
   #姓、名、メール、パスワードがあれば有効な状態であること
   it "is valid with a first name, last name and email, and password" do
