41d23b45f64772d9d2f2d998a28035513fb768a2
Abiti0233 <gogoyon3021@gmail.com>
2023-10-20 14:46:06 +0900

    add chapter8

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -11,7 +11,10 @@ RSpec.describe ProjectsController, type: :controller do
       it "responds successfully" do
         sign_in @user
         get :index
-        expect(response).to be_successful
+        aggregate_failures do
+          expect(response).to be_successful
+          expect(response).to have_http_status "200"
+        end
       end
 
       #200レスポンスを返すこと
